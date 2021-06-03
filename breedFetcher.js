a77af3d9d9945a30d0cb972d8e14cd2706524457
Pegasusor001 <linus.ji.001@gmail.com>
2021-06-03 16:18:27 -0700

    finally finished

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,26 @@
-const fs = require('fs');
 const request = require('request');
-const arg = process.argv;
 
-const breedName = arg[2];
-const domain = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request(domain, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
-  const data = JSON.parse(body);
-  if (data.length === 0){
-    console.log ('The request breed name is not found.')
-  } else {
-    console.log(data[0]['description']);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const domain = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  
+  request(domain, (error, response, body) => {
+    if (error) {
+      callback (error, null);
+    }
+    // console.log(error);
+    const data = JSON.parse(body)[0];
+    
+    if (data){
+      callback(null, data.description);
+    } else {
+      callback("Error, It\'s not found", null);
+    }
+    
+    
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
 
